70e204d7a1a95040ab050e08b2ae405b1e84918a
Armynerh <aimenah@outlook.com>
2025-09-21 14:59:02 -0400

    task ten

---
+++ 0x04-TypeScript/task_4/js/subjects/main.ts
@@ -0,0 +1,37 @@
+/// <reference path="./Teacher.ts" />
+/// <reference path="./Subject.ts" />
+/// <reference path="./Cpp.ts" />
+/// <reference path="./React.ts" />
+/// <reference path="./Java.ts" />
+
+import Subjects = Subjects; // Import the namespace
+
+// Create teacher object with experience in C
+export const cTeacher: Subjects.Teacher = {
+  firstName: "Guillaume",
+  lastName: "Salva",
+  experienceTeachingC: 10
+};
+
+// Create and export subject instances
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+
+// Cpp
+console.log("C++");
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+// Java
+console.log("Java");
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+// React
+console.log("React");
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
